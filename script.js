c63f007e899bee2721488f758fc31d6f377fa112
DosSantos-Guilherme <gui_santoos15@hotmail.com>
2024-02-07 01:01:08 -0300

    Complete problem statement 1

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function calculateArea() {
-  const PI = 3.141592
+function calculateCircunference() {
   let radius = document.getElementById("radiusV").value
-  document.getElementById("result").innerHTML = radius ** 2 * Math.PI
+  let areaC = radius ** 2 * Math.PI
+  let circC = 2 * Math.PI * radius
+  let packsC = Math.round(circC / 20)
+  document.getElementById("result").innerHTML =
+    "Area = " +
+    areaC +
+    "<br>" +
+    "Circunferência = " +
+    circC +
+    "<br>" +
+    "Quantidade de arames=" +
+    packsC
 }
 document.getElementById("radiusV").addEventListener("keydown", function (e) {
   if (e.code == "Enter") {
-    calculateArea()
+    calculateCircunference()
   }
 })
